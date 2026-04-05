4f2e97687f505950080d8794192de1efeae16b63
Nabil <abubakar.nabil.210@gmail.com>
2026-04-05 22:10:53 +0100

    Feat: Scrummy bug

--- backend/order-processor/index.js
+++ backend/order-processor/index.js
@@ -3,7 +3,7 @@ const amqp = require('amqplib');
 require('dotenv').config();
 const client = require('prom-client');
 const app = express();
-const port = process.env.ORDER_PROCESSOR_PORT || 3001;
+const port = process.env.PORT || 3003;
 
 // Prometheus Registry
 const register = new client.Registry();
